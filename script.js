b7132b964bbcb4618e85464eec7b30721cdb221d
TomKingori <tomkingori66@gmail.com>
2022-07-02 12:07:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function track(){
+    var nameval = document.getElementById("name").value;
+    var dateval = document.getElementById("date").value;
+    var amountval = document.getElementById("amount").value;
+
+    
+};
